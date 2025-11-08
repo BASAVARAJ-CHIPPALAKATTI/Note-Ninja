63531d56d93fadbfeffcd8056f11c7ac9e0b2661
Basavaraj Chippalakatti <bchippalakatti105@gmail.com>
2025-11-08 23:03:33 +0530

    correct the some error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://note-ninja-168a.onrender.com";
+const API_BASE_URL = "https://note-ninja-168a.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
